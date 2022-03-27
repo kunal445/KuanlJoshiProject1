eec2c1be688d1078b0470d8de29d4681d6ea280a
kunal445 <73367589+kunal445@users.noreply.github.com>
2022-03-27 18:21:15 -0400

    fixed some padding and margins to nav menu

--- app.js
+++ app.js
@@ -2,16 +2,17 @@
 // add an eventlistners to it
 // when you click it will toggle a class on the ul
 
-
+// code for the hamburger menu
 const hamburger = document.querySelector('.hamburgerMenu');
 
 hamburger.addEventListener('click', function(e){
     e.preventDefault();
 
-    const ulElement = document.querySelector('nav ul')
+    const ulElement = document.querySelector('header ul')
     ulElement.classList.toggle('hideMe');
     ulElement.classList.toggle('showMe');
 })
 
 
 
+// create the comments
